ee90882873ad31a6b111c03e0faa093998bb0418
MagusDei <jami.valorinta@helsinki.fi>
2017-05-15 14:29:59 +0300

    Updates to apirequest.r

--- ecco_wordcounter/apirequest.r
+++ ecco_wordcounter/apirequest.r
@@ -1,4 +1,18 @@
 library(jsonlite)
-word <- "china"
-mydata <- fromJSON(paste("https://vm0542.kaj.pouta.csc.fi/ecco_octavo_api/search?query=", word, "&limit=-1&field=ESTCID", sep=""))$results$docs
-mydata
+library(ggplot2)
+
+mycsv <- read.csv("estc_processed_080517.csv")
+
+cleaner <- function(id) {
+  return(gsub("^([A-Z])0*([1-9][0-9]*)$","\\1\\2",id))
+}
+
+counter <- function(word) {
+  mydata <- fromJSON(paste("https://vm0542.kaj.pouta.csc.fi/ecco_octavo_api/search?query=", word, "&limit=-1&field=ESTCID&field=documentLength", sep=""))$results$docs
+  publication_year <- mycsv[match(sapply(mydata$ESTCID, cleaner), mycsv$id), 'publication_year']
+  return(cbind(mydata, publication_year))
+}
+
+finaldata <- counter("china")
+finaldatalimited <- subset(finaldata, publication_year >= 1700 & publication_year <= 1800)
+ggplot(data=finaldatalimited, aes(publication_year, score)) + geom_bar(stat="identity")
