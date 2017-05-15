9853b2d2fc098ddc0c0e3aa2cec24830f4aa2bd5
Ville Vaara <villepvaara@gmail.com>
2017-05-15 16:25:44 +0300

    metadata creation script added

--- metadata.R
+++ metadata.R
@@ -1,18 +1,33 @@
 library(jsonlite)
 
+cleaner <- function(id) {
+  return(gsub("^([A-Z])0*([1-9][0-9]*)$","\\1\\2",id))
+}
+
 estc_metadata <- read.csv("../data/estc_processed_080517.csv")
 ecco_metadata <- fromJSON("../data/api3dump.json")
 
-querydata <- fromJSON("https://vm0542.kaj.pouta.csc.fi/ecco_octavo_api/search?query=king&pretty=&timeout=-1&field=ESTCID")$results$docs
 
-querydata$year <-
-  estc_metadata[match(querydata$ESTCID, estc_metadata$id),"publication_year"]
+ecco_metadata$pub_year <-
+  estc_metadata[match(cleaner(ecco_metadata$ESTCID),
+                      estc_metadata$id),"publication_year"]
 
-query <- "https://vm0542.kaj.pouta.csc.fi/ecco_octavo_api/search?query=<DOCUMENT§china§DOCUMENT>&pretty=&timeout=-1&field=ESTCID"
 
-geo_names <- fromJSON("../data/tgn_7000874.json")
-g2 <- geo_names$results$bindings
+retdata <- data.frame(year = numeric(0), pages = numeric(0),
+                      paragraphs = numeric(0), characters = numeric(0))
 
-cities <- read.csv("../data/worldcitiespop.csv")
-cities_matti <- read.csv("../data/dictionary_geo_entry.csv")
-cities_matti_unique <- unique(cities_matti)
+for (qyear in 1700:1799) {
+  # print(year)
+  year_subset <- subset(ecco_metadata, ecco_metadata$pub_year == qyear)
+  pages_sum = sum(year_subset$totalPages)
+  para_sum = sum(year_subset$totalParagraphs)
+  char_sum = sum(year_subset$documentLength)
+
+  year_df <- data.frame(year = qyear, pages = pages_sum,
+                        paragraphs = para_sum, characters = char_sum)
+  retdata <- rbind(retdata, year_df)
+    
+}
+
+write.csv(retdata, "yearly_ecco_summaries.csv", row.names = F)
+
